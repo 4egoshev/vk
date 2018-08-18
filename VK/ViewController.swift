40d9495f246ac67ea509b04499b339783283d88b
Александр Чегошев <4egoshev@gmail.com>
2018-08-18 23:30:00 +0700

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Александр Чегошев on 18.08.2018.
+//  Copyright © 2018 4egoshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
