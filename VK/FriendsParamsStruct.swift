45d7002cd995ffdafe6bd5532404d2908e8b4c44
Александр Чегошев <4egoshev@gmail.com>
2019-01-29 11:18:29 +0700

    Модель

---
+++ VK/FriendsParamsStruct.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsParamsStruct.swift
+//  VK
+//
+//  Created by Александр Чегошев on 19.08.2018.
+//  Copyright © 2018 4egoshev. All rights reserved.
+//
+
+import Foundation
+
+struct FriendsParamsStruct {
+    
+    let user_id: String
+    let order: String
+    let count: Int
+    let fields: String
+    let nameCase: String
+}
